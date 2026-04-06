2a4643ccdcc59c0395a4748f45e8e6622e175752
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-04-07 02:01:59 +0530

    Add create note, save, and delete features

--- script.js
+++ script.js
@@ -2,17 +2,51 @@ const notesContainer = document.querySelector(".notesContainer");
 const createBtn = document.querySelector(".addNoteBtn");
 let notes = document.querySelectorAll(".inputBox");
 
+// Load notes from local storage
+function loadNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+
+// Local Storage Update
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+// Load notes on page load
+loadNotes();
+
+// Add new note
 createBtn.addEventListener("click", () => {
+    let noteWrapper = document.createElement("div");
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
+
+    noteWrapper.className = "note";
+
     inputBox.className = "inputBox";
     inputBox.setAttribute("contenteditable", "true");
+
     img.src = "Images/delete.png";
     img.className = "deleteIcon";
-    inputBox.appendChild(img);
-    notesContainer.appendChild(inputBox);
 
-    img.addEventListener("click", () => {
-        notesContainer.removeChild(inputBox);
-    })
+    noteWrapper.appendChild(inputBox);
+    noteWrapper.appendChild(img);
+
+    notesContainer.appendChild(noteWrapper);
+
+    updateStorage();
 })
+
+// Delete note
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.classList.contains("deleteIcon")) {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+});
+
+// Save on typing
+notesContainer.addEventListener("input", function() {
+    updateStorage();
+});
+
